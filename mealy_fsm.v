af130781655dfdcff24f6b5015fab99d0b057ab3
Bob <Bob@Bobs-MacBook-Pro.local>
2014-04-05 23:25:18 -0600

    added mealy fsm

--- mealy_fsm.v
+++ mealy_fsm.v
@@ -1,6 +1,7 @@
-/* Code for a Moore FSM that recognizes the sequence ABBAC through
- * button presses on a DE0
+/* This code is a Mealy FSM that recongnizes the sequence ABBAC 
+ * created by pushing buttons on a DE0
 */
+
 
 /* Module that creates the button pulses */
 module flipflop(Button, Clock, Q);
@@ -19,10 +20,11 @@ end
 assign Q = Q2&~Q3;
 endmodule
 
-module moore(Clock, Resetn, Button, z, z1, z2, z3, z4, z5);
+module mealy(Clock, Resetn, Button, z, z0, z1, z2, z3, z4, z5);
 input Clock, Resetn;
 input [2:0] Button;
-output z, z1, z2, z3, z4, z5;
+output z0, z1, z2, z3, z4, z5;
+output reg z;
 wire A, B, C;
 reg [2:0] y;
 reg [2:0] r;
@@ -51,54 +53,91 @@ flipflop three(r[0], Clock, C);
 always@(posedge Clock or negedge Resetn)
 begin
 	if(Resetn == 0)
+	begin
 		y <= S0;
+		z <= 0;
+	end
 	else
 		case(y)
 			S0:
 				if(A)
+				begin
 					y <= S1;
-				else if(A||B)
+					z <= 0;
+				end
+				else if(B||C)
+				begin
 					y <= S0;
+					z <= 0;
+				end
 			S1:
 				if(B)
+				begin
 					y <= S2;
+					z <= 0;
+				end
 				else if(A)
+				begin
 					y <= S1;
+					z <= 0;
+				end
 				else if(C)
+				begin
 					y <= S0;
+					z <= 0;
+				end
 			S2:
 				if(B)
+				begin	
 					y <= S3;
-				else if(A)
-					y <= S1;
+					z <= 0;
+				end
 				else if(C)
+				begin
 					y <= S0;
+					z <= 0;
+				end
+				else if(A)
+				begin
+					y <= S1;
+					z <= 0;
+				end
 			S3:
 				if(A)
+				begin
 					y <= S4;
+					z <= 0;
+				end
 				else if(B||C)
+				begin
 					y <= S0;
+					z <= 0;
+				end
 			S4:
 				if(C)
-					y <= S5;
-				else if(B)
-					y <= S2;
-				else if(A)
-					y <= S1;
-			S5:
-				if(A)
-					y <= S1;
-				else if(B||C)
+				begin
 					y <= S0;
+					z <= 1;
+				end
+				else if(A)
+				begin
+					y <= S1;
+					z <= 0;
+				end
+				else if(B)
+				begin
+					y <= S2;
+					z <= 0;
+				end
 		endcase
 end
 
 /* Led outputs for the different states */
-assign z = (y == S5);
-assign z1 = (y == S4);
-assign z2 = (y == S3);
-assign z3 = (y == S2);
-assign z4 = (y == S1);
-assign z5 = (y == S0);
+assign z0 = (y == S0);
+assign z1 = (y == S1);
+assign z2 = (y == S2);
+assign z3 = (y == S3);
+assign z4 = (y == S4);
+assign z5 = (y == S5);
 
 endmodule
